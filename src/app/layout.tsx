1bb04e4342c75050eee506d71fe7be7d640d7389
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2024-04-27 16:12:50 +0800

    feat:wheretodance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import NavBar from "@/components/navbar";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +23,16 @@ export default function RootLayout({
       <head>
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="fixed top-0 left-0 right-0 z-10 bg-black shadow-md h-16">
+          <NavBar />
+        </div>
+        <div className="pt-16">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
